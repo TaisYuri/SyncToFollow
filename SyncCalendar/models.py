11bc62cb07009ddbdedd196e698509f4aa3a2dab
Tais Yuri <taisyuri@hotmail.com>
2023-09-03 14:26:58 -0300

    codloja

--- SyncCalendar/models.py
+++ SyncCalendar/models.py
@@ -12,17 +12,16 @@ class Schedule(models.Model):
     createdDate = models.DateTimeField(auto_now=True)
 
 class UpdateRegister(models.Model):
-    codLoja = models.CharField(max_length=5, null=False, blank=False)
+    codLoja = models.CharField(max_length=5, primary_key=True)  # Definindo codLoja como chave primária
     platFiscal = models.CharField(max_length= 255, null= False, blank= False)
     cadBanco = models.BooleanField(default=False, null=False, blank=False)
     cadRF = models.BooleanField(default=False,  null=False, blank=False)
-    csc_acSat = models.CharField(max_length=255, null=False, blank=False)
-    certDigital_atvSat = models.CharField(max_length=255, null=False, blank=False)
+    csc_acSat = models.CharField(max_length=255)
+    certDigital_atvSat = models.CharField(max_length=255)
     impostos = models.BooleanField(default=True,  null=False, blank=False)
     check_status = models.BooleanField(default=False,  null=False, blank=False)
     steps = models.JSONField(encoder=None)
-    fileCsc_acSat = models.FileField( upload_to='uploads/', blank=True)
-    fileCertDigital_atvSat = models.FileField(  null=True, blank=True)
+   
 
 class UpdateImage(models.Model):
     codLoja = models.ForeignKey(to= UpdateRegister, on_delete= models.CASCADE, null= False, blank= False)
